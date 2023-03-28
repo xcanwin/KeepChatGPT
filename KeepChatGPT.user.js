01fee2e296169cbac2c4e664f255c126e91af2a8
xcanwin <xcanwin@gmail.com>
2023-03-29 04:22:38 +0800

    修复CF验证下还继续维持的bug

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KeepChatGPT
-// @description  ChatGPT维持会话
-// @version      1.1
+// @description  ChatGPT网络异常拜拜员. Chatgpt no longer network error. 
+// @version      1.2
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/KeepChatGPT/
 // @supportURL   https://github.com/xcanwin/KeepChatGPT/
@@ -17,22 +17,24 @@
 
     window.ifrurl = "/api/auth/session";
     var loadifr = function() {
-        if(!window.ifrid){
-            var ifr = document.createElement('iframe');
-            ifr.id = "ifrid";
-            ifr.style.display = 'none';
-            ifr.src = window.ifrurl;
-            ifr.style.width = document.querySelectorAll("nav a")[0].offsetWidth + 'px';
-            ifr.onload = function() {
-                try {
-                    ifr.contentWindow.document.body.style.background = '#555';
-                    console.log(`KeepChatGPT: ${JSON.parse(ifr.contentDocument.body.innerText)['expires']}`);
-                } catch (e) {
-                }
-            };
-            document.querySelectorAll("nav")[0].appendChild(ifr);
-        } else{
-            window.ifrid.src = window.ifrurl;
+        if(document.querySelector("nav a")){
+            if(!window.ifrid){
+                var ifr = document.createElement('iframe');
+                ifr.id = "ifrid";
+                ifr.style.display = 'none';
+                ifr.src = window.ifrurl;
+                ifr.style.width = document.querySelector("nav a").offsetWidth + 'px';
+                ifr.onload = function() {
+                    try {
+                        ifr.contentWindow.document.body.style.background = '#555';
+                        console.log(`KeepChatGPT: ${JSON.parse(ifr.contentDocument.body.innerText)['expires']}`);
+                    } catch (e) {
+                    }
+                };
+                document.querySelector("nav").appendChild(ifr);
+            } else{
+                window.ifrid.src = window.ifrurl;
+            }
         }
     }
 
@@ -45,18 +47,20 @@
     }
 
     setInterval(function() {
-        if(!window.ndivid){
-            loadifr();
-            var ndiv = document.createElement("div");
-            ndiv.id = "ndivid";
-            ndiv.setAttribute("class", document.querySelector("nav a").className);
-            ndiv.innerHTML = `<svg width="16" height="16"></svg>KeepChatGPT`;
-            ndiv.onclick = window.kcg;
-            var nav = document.querySelector('nav');
-            nav.insertBefore(ndiv, nav.childNodes[1]);
-            var newstyle = document.createElement('style');
-            newstyle.innerHTML = `#ndivid{animation:color-animation 6s ease-in-out infinite;color:black;}@keyframes color-animation{0%{background-color:#F0B27A;}50%{background-color:#FDE184;}100%{background-color:#F0B27A;}}`;
-            document.body.appendChild(newstyle);
+        if(document.querySelector("nav a")){
+            if(!window.ndivid){
+                loadifr();
+                var ndiv = document.createElement("div");
+                ndiv.id = "ndivid";
+                ndiv.setAttribute("class", document.querySelector("nav a").className);
+                ndiv.innerHTML = `<svg width="16" height="16"></svg>KeepChatGPT`;
+                ndiv.onclick = window.kcg;
+                var nav = document.querySelector('nav');
+                nav.insertBefore(ndiv, nav.childNodes[1]);
+                var newstyle = document.createElement('style');
+                newstyle.innerHTML = `#ndivid{animation:color-animation 6s ease-in-out infinite;color:black;}@keyframes color-animation{0%{background-color:#F0B27A;}50%{background-color:#FDE184;}100%{background-color:#F0B27A;}}`;
+                document.body.appendChild(newstyle);
+            }
         }
     }, 300);
 
