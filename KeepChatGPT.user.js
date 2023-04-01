86436090219eb3a07ea699526ea1c96ee4c63fa6
xcanwin <xcanwin@gmail.com>
2023-04-02 03:14:02 +0800

    更新说明描述

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
-// @name         KeepChatGPT
-// @description  完美解决ChatGPT网络错误, 不再频繁地刷新网页, 非常顺畅. 解决了这几类报错: (1) NetworkError when attempting to fetch resource, (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com.
-// @version      2.5
-// @author       xcanwin
-// @namespace    https://github.com/xcanwin/KeepChatGPT/
-// @supportURL   https://github.com/xcanwin/KeepChatGPT/
-// @updateURL    https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js
-// @downloadURL  https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js
-// @icon         data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" stroke-width="2" fill="none" stroke="currentColor"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-// @license      GPL-2.0-only
-// @match        *://chat.openai.com/chat*
-// @grant        none
-// @run-at       document-idie
+// @name             KeepChatGPT
+// @description      让我们在使用ChatGPT过程中更高效、更顺畅，完美解决ChatGPT网络错误，不再频繁地刷新网页，足足省去10个多余的步骤。解决了这几类报错: （1）NetworkError when attempting to fetch resource. （2）Something went wrong. If this issue persists please contact us through our help center at help.openai.com.
+// @version          2.6
+// @author           xcanwin
+// @description:en   Let's make our use of ChatGPT more efficient and smoother, by perfecting the solution to ChatGPT network errors. This saves us from frequently refreshing the webpage and eliminates over 10 unnecessary steps. The following errors have been resolved: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com.
+// @namespace        https://github.com/xcanwin/KeepChatGPT/
+// @supportURL       https://github.com/xcanwin/KeepChatGPT/
+// @updateURL        https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js
+// @downloadURL      https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js
+// @icon             data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" stroke-width="2" fill="none" stroke="currentColor"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+// @license          GPL-2.0-only
+// @match            *://chat.openai.com/chat*
+// @grant            none
+// @run-at           document-idie
 // ==/UserScript==
 
 
