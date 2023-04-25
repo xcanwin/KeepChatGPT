bcb97308cf282667d81b393e1d0269e4ae1c65f4
EzraChan <26107204+Ezra-Chan@users.noreply.github.com>
2023-04-25 17:42:21 +0800

    fix: version comparison error
    Change-Id: I60eb1a8280e68b5592040068311f94c861c0b1c7

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       让我们在使用ChatGPT过程中更高效、更顺畅，完美解决ChatGPT网络错误，不再频繁地刷新网页，足足省去10个多余的步骤。还可以取消后台监管审计。解决了这几类报错: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) This content may violate our content policy. If you believe this to be in error, please submit your feedback — your input will aid our research in this area. (4) Conversation not found.
-// @version           10.1
+// @version           10.0
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -571,7 +571,7 @@ nav {
             response.text().then((data) => {
                 const m = data.match(/@version\s+(\S+)/);
                 const ltv = m && m[1];
-                if (ltv && ltv > crv) {
+                if (ltv && parseFloat(ltv) > parseFloat(crv)) {
                     ndialog(`${tl("检查更新")}`, `${tl("当前版本")}: ${crv}, ${tl("发现最新版")}: ${ltv}`, `UPDATE`, function(t) {
                         window.open(updateURL, '_blank');
                     });
