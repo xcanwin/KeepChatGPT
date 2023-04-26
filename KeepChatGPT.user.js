ad0f9ac94cc10b7487dc8865a8631587509381b1
EzraChan <26107204+Ezra-Chan@users.noreply.github.com>
2023-04-26 17:21:19 +0800

    fix: When clicking on some options, the menu should be closed
    Change-Id: I24d2abb9151c0e1d0d11d4af601a190f97c8e6c4

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       让我们在使用ChatGPT过程中更高效、更顺畅，完美解决ChatGPT网络错误，不再频繁地刷新网页，足足省去10个多余的步骤。还可以取消后台监管审计。解决了这几类报错: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) This content may violate our content policy. If you believe this to be in error, please submit your feedback — your input will aid our research in this area. (4) Conversation not found.
-// @version           10.0
+// @version           10.1
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -293,6 +293,7 @@
             $('.checkbutton', this).classList.toggle('checked');
         };
         $('#nmenuid_af').onclick = function() {
+            toggleMenu();
             ndialog(`${tl("调整间隔")}`, `${tl("建议间隔30秒")}`, `Go`, function(t) {
                 try {
                     interval2Time = parseInt($(".kdialoginput", t).value);
@@ -308,11 +309,23 @@
             }, `input`, parseInt(gv("k_interval", 30)));
         };
         $('#nmenuid_cu').onclick = function() {
+            toggleMenu();
             checkForUpdates();
         };
         $('#nmenuid_ab').onclick = function() {
             window.open(GM_info.script.namespace, '_blank');
         };
+    };
+
+    const toggleMenu = function() {
+        const ndivmenu = $(".kmenu");
+        if (ndivmenu.style.display === 'none') {
+            ndivmenu.style.display = 'block';
+            ndivmenu.style.left = `${$("#kcg").getBoundingClientRect().right + 20}px`;
+            ndivmenu.style.top = `${$("#kcg").getBoundingClientRect().top}px`;
+        } else {
+            ndivmenu.style.display = 'none';
+        }
     };
 
     const loadKCG = function() {
@@ -350,15 +363,7 @@
         ndivkcg.onmouseleave = ndivmenu.onmouseleave = function() {
             ndivmenu.style.display = 'none';
         };
-        ndivkcg.onclick = function() {
-            if (ndivmenu.style.display === 'none') {
-                ndivmenu.style.display = 'block';
-                ndivmenu.style.left = `${$("#kcg").getBoundingClientRect().right + 20}px`;
-                ndivmenu.style.top = `${$("#kcg").getBoundingClientRect().top}px`;
-            } else {
-                ndivmenu.style.display = 'none';
-            }
-        };
+        ndivkcg.onclick = toggleMenu;
         const icon = GM_info.script.icon ? GM_info.script.icon : `${GM_info.script.namespace}raw/main/assets/logo.svg`;
         ndivkcg._symbol1_innerHTML = `<img src='${icon}' />Keep${ndivkcg.id.slice(1,2).toUpperCase()}hatGPT by x${ndivkcg.id.slice(1,2)}anwin`;
         ndivkcg._symbol2_innerHTML = `Keep${ndivkcg.id.slice(1,2).toUpperCase()}hatGPT`;
