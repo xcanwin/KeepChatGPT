df95cc562d1004a6fd78b6b894e25bdd940e4616
EzraChan <26107204+Ezra-Chan@users.noreply.github.com>
2023-05-05 10:23:12 +0800

    fix: the width of modal is abnormal
    Change-Id: Icad425b52cbd3898185e395ebc58896e6c49c2a6

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       ChatGPT畅聊插件。解决所有报错，让我们的AI体验无比顺畅、丝滑、高效。持续更新的增强功能，如取消审计等。解决的报错如下: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) Conversation not found. (4) This content may violate our content policy.
-// @version           11.11
+// @version           11.12
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -533,7 +533,7 @@
     cursor: pointer;
 }
 
-div.items-end>div:first-child {
+.group div.items-end>div:first-child {
     user-select: none;
     max-width: 30px;
     cursor: pointer;
