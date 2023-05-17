041240a1a3dc69187b31bd789300419a3df3417d
未知可行 <xcanwin@gmail.com>
2023-05-17 13:15:25 +0800

    修复取消审计功能

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一个ChatGPT的畅聊与增强插件。开源免费。不仅能解决所有报错不再刷新，还有保持活跃、取消审计、克隆对话、净化首页、展示大屏、展示全屏、言无不尽等多个高级功能。让我们的AI体验无比顺畅、丝滑、高效、简洁。解决的报错如下: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) Conversation not found. (4) This content may violate our content policy.
-// @version           13.1
+// @version           13.2
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -702,9 +702,19 @@ nav {
         if (action === true) {
             unsafeWindow.fetch = new Proxy(fetch, {
                 apply: function (target, thisArg, argumentsList) {
-                    const n = {};
-                    n.json = function() {return {};};
-                    return argumentsList[0].includes('moderations') ? Promise.resolve(n) : target.apply(thisArg, argumentsList);
+                    try {
+                        if (argumentsList[0].includes('moderations')) {
+                            const n = {};
+                            n.json = function() {return {};};
+                            return Promise.resolve(n);
+                        } else if (argumentsList[0].includes('conversation')) {
+                            const post_body = JSON.parse(argumentsList[1].body);
+                            post_body.supports_modapi = false;
+                            argumentsList[1].body = JSON.stringify(post_body);
+                            return target.apply(thisArg, argumentsList);
+                        }
+                    } catch (e) {}
+                    return target.apply(thisArg, argumentsList);
                 }
             });
         } else {
