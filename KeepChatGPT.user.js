01d1a634cd2705e380cb9276b4195916ab957270
未知可行 <xcanwin@gmail.com>
2023-06-15 14:33:49 +0800

    让gpt4的高亮显得友好

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一个ChatGPT的畅聊与增强插件。开源免费。不仅能解决所有报错不再刷新，还有保持活跃、取消审计、克隆对话、净化首页、展示大屏、展示全屏、言无不尽、拦截跟踪、日新月异等多个高级功能。让我们的AI体验无比顺畅、丝滑、高效、简洁。解决的报错如下: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) Conversation not found. (4) This content may violate our content policy.
-// @version           15.4
+// @version           15.5
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -985,8 +985,8 @@ nav.flex div.overflow-y-auto {
                 $('.navlast', el).innerHTML = last;
             }
             if (model.match('gpt-4')) {
-                el.style.color = `#FF4CFF`;
-                $('.navlast', el).style.color = `#C86AC8`;
+                $('svg:first-child', el).setAttribute("stroke", `#FF4CFF`);
+                $('svg:first-child', el).setAttribute("fill", `#FF4CFF`);
             }
         });
 
