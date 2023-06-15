d9739d9f425484cee3f4373001f1073c5c8c8a91
未知可行 <xcanwin@gmail.com>
2023-06-15 15:06:21 +0800

    修复来自chatgpt回复的xss漏洞

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一个ChatGPT的畅聊与增强插件。开源免费。不仅能解决所有报错不再刷新，还有保持活跃、取消审计、克隆对话、净化首页、展示大屏、展示全屏、言无不尽、拦截跟踪、日新月异等多个高级功能。让我们的AI体验无比顺畅、丝滑、高效、简洁。解决的报错如下: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) Conversation not found. (4) This content may violate our content policy.
-// @version           15.5
+// @version           15.6
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -294,6 +294,13 @@
             return d;
         }
     };
+
+    const htmlEncode = function(text) {
+        var tempElement = document.createElement("div");
+        var textNode = document.createTextNode(text);
+        tempElement.appendChild(textNode);
+        return tempElement.innerHTML;
+    }
 
     const setIfr = function(u = "") {
         if ($("#xcanwin") === null) {
@@ -975,14 +982,14 @@ nav.flex div.overflow-y-auto {
 </div>
 <br>
 <div style="max-height: unset; max-width: 95%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; color: gray; font-size: 0.75rem;" class="navlast">
-    ${last}
+    ${htmlEncode(last)}
 </div>
 `;
                 el.insertBefore(cdiv_new, el.childNodes[1]);
             } else if ($('.navtitle', el).innerHTML !== title || $('.navdate', el).innerHTML !== formatDate2(update_time) || $('.navlast', el).innerHTML !== last) {
                 $('.navtitle', el).innerHTML = title;
                 $('.navdate', el).innerHTML = formatDate2(update_time);
-                $('.navlast', el).innerHTML = last;
+                $('.navlast', el).innerHTML = htmlEncode(last);
             }
             if (model.match('gpt-4')) {
                 $('svg:first-child', el).setAttribute("stroke", `#FF4CFF`);
@@ -1021,7 +1028,7 @@ nav.flex div.overflow-y-auto {
             global.kec_object[crt_con_id].model = crt_con_model;
             const crt_st_ec = await global.st_ec.get(crt_con_id);
             await global.st_ec.put({id: crt_con_id, title: crt_st_ec.title, update_time: crt_st_ec.update_time, last: crt_con_last, model: crt_con_model});
-            $('.navlast', crt_con_a).innerHTML = crt_con_last;
+            $('.navlast', crt_con_a).innerHTML = htmlEncode(crt_con_last);
         }
     };
 
