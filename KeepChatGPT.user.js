5227f7a1801b9bf8ceb93fd7c476877601ece73e
未知可行 <xcanwin@gmail.com>
2023-06-15 11:07:59 +0800

    提高日新月异功能最新消息的刷新速度

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一个ChatGPT的畅聊与增强插件。开源免费。不仅能解决所有报错不再刷新，还有保持活跃、取消审计、克隆对话、净化首页、展示大屏、展示全屏、言无不尽、拦截跟踪、日新月异等多个高级功能。让我们的AI体验无比顺畅、丝滑、高效、简洁。解决的报错如下: (1) NetworkError when attempting to fetch resource. (2) Something went wrong. If this issue persists please contact us through our help center at help.openai.com. (3) Conversation not found. (4) This content may violate our content policy.
-// @version           15.1
+// @version           15.2
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -1010,9 +1010,10 @@ nav.flex div.overflow-y-auto {
             crt_con_last = [...$$("main div.text-base .markdown")].pop().innerText.trim().replace(/[\r\n]/g, ``).substr(0, 100);
         }
         if (crt_con_id && global.kec_object[crt_con_id] && crt_con_last && global.kec_object[crt_con_id].last !== crt_con_last) {
+            global.kec_object[crt_con_id].last = crt_con_last;
             const crt_st_ec = await global.st_ec.get(crt_con_id);
             await global.st_ec.put({id: crt_con_id, title: crt_st_ec.title, update_time: crt_st_ec.update_time, last: crt_con_last});
-            global.kec_object[crt_con_id].last = crt_con_last;
+            $('.navlast', crt_con_a).innerHTML = crt_con_last;
         }
     };
 
