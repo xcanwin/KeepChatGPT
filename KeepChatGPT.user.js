fddd4bec8487e83c6b908729ce5f74648f50bbcd
未知可行 <xcanwin@gmail.com>
2023-09-11 11:37:23 +0800

    Update KeepChatGPT.user.js
    新增净化This advanced data analysis (beta) chat has timed out

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化首页、展示大屏、展示全屏、拦截跟踪、日新月异等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           17.11
+// @version           17.12
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -1186,6 +1186,7 @@ nav.flex .transition-all {
             if (utp_svg && findParent(utp_svg, `a`, 4)) {
                 findParent(utp_svg, `a`, 4).classList.add('hide');
             }
+            $(`div[portal-container=""] .toast-root`).classList.add('hide');
         }
     };
 
