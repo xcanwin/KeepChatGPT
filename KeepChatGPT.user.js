34068d9bf23e31a15bc94065672757c434488e34
未知可行 <xcanwin@gmail.com>
2023-11-22 16:43:08 +0800

    美化移动端的样式，解决部分报错

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、展示全屏、拦截跟踪、日新月异等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           21.0
+// @version           21.1
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -580,7 +580,7 @@
                 location.reload();
             } else {
                 sv("k_everchanging", true);
-                $('nav.flex').classList.add('knav');
+                $('nav.flex')?.classList.add('knav');
                 everChanging();
             }
             $('.checkbutton', this).classList.toggle('checked');
@@ -647,7 +647,7 @@
 
         if (gv("k_everchanging", false) === true) {
             $('#nmenuid_ec .checkbutton').classList.add('checked');
-            $('nav.flex').classList.add('knav');
+            $('nav.flex')?.classList.add('knav');
             everChanging();
         }
 
@@ -689,6 +689,7 @@
             } else if ($(symbol2_selector)) {
                 kcg_html.innerHTML = kcg_html._symbol2_innerHTML;
                 symbol_prt = findParent($(symbol2_selector), ".sticky", 2);
+                $(symbol2_selector).parentNode.classList.remove('absolute');
             }
             symbol_prt.insertBefore(kcg_html, symbol_prt.childNodes[0]);
             return;
@@ -725,6 +726,7 @@
         } else if ($(symbol2_selector)) {
             ndivkcg.innerHTML = ndivkcg._symbol2_innerHTML;
             symbol_prt = findParent($(symbol2_selector), ".sticky", 2);
+            $(symbol2_selector).parentNode.classList.remove('absolute');
         }
         kcg_html = ndivkcg;
         symbol_prt.insertBefore(kcg_html, symbol_prt.childNodes[0]);
@@ -751,7 +753,8 @@
 }
 
 #kcg {
-    margin-top: .875rem;
+    margin-top: .5rem;
+    margin-bottom: .5rem;
 }
 #kcg::before {
     content: '';
