934d026d957df7816cd98dd9f79e0579c6090dcc
xcanwin <14187349+xcanwin@users.noreply.github.com>
2024-05-11 12:05:39 +0800

    修复对话框没有居中的bug

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、展示全屏、拦截跟踪、日新月异等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           24.9
+// @version           25.1
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -916,6 +916,7 @@ nav div.pt-3\\.5 {
         }
         form.w-full {
             max-width: 85%;
+            margin: auto;
         }
     }
     img {
