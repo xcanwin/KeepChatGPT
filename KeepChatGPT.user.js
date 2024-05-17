c333030c1a28b2442207e4c876dec73df2169f08
xcanwin <14187349+xcanwin@users.noreply.github.com>
2024-05-17 23:44:02 +0800

    openai官方的语言功能已无bug，恢复官方功能

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、展示全屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           27.8
+// @version           27.9
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -1370,7 +1370,7 @@ nav.flex .transition-all {
     };
 
     hookFetch();
-    fixOpenaiBUG();
+    //fixOpenaiBUG();
 
     let nInterval1 = setInterval(nInterval1Fun, 300);
 
