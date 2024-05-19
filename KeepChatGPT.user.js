063ebd89cede0d6db30780676aa8fe25a36945fc
xcanwin <14187349+xcanwin@users.noreply.github.com>
2024-05-19 23:51:50 +0800

    提升明察秋毫的视觉体验

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、展示全屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           27.9
+// @version           28.1
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -916,9 +916,15 @@ main .text-sm.flex-col>.text-token-text-primary .mx-auto>div:first-child {
 
 /*明察秋毫*/
 .kkeenobservation {
+    /*用户气泡优化*/
     main div[data-message-author-role="user"] {
         padding-right: 3rem;
     }
+    main div[data-message-author-role="user"]>div.w-full>div {
+        background-color: #deedd7;
+    }
+
+    /*添加用户头像*/
     main div[data-message-author-role="user"]::after {
         content: '';
         position: absolute;
@@ -930,13 +936,39 @@ main .text-sm.flex-col>.text-token-text-primary .mx-auto>div:first-child {
         background-size: contain;
         border-radius: 50%;
     }
+
+    /*用户气泡下标优化*/
     main .text-token-text-primary .juice\\:flex-row-reverse .rounded-xl {
         padding-right: 2.5rem;
     }
+
+    /*机器人气泡优化*/
     main div[data-message-author-role="assistant"] {
         padding-right: 3.5rem;
     }
+    main div[data-message-author-role="assistant"]>div.w-full {
+        align-items: flex-start;
+    }
+    main div[data-message-author-role="assistant"]>div.w-full>div {
+        width: auto;
+        max-width: 100%;
+        border-radius: 1.5rem;
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+        padding-left: 1.25rem;
+        padding-right: 1.25rem;
+        background-color: var(--main-surface-secondary);
+    }
 }
+/*官方暗色模式*/
+.dark {
+    .kkeenobservation {
+        main div[data-message-author-role="user"]>div.w-full>div {
+            background-color: #525452;
+        }
+    }
+}
+
 
 
 nav {
