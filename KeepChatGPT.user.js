51b3aa4c45149119e9570d6c22b5fd54d007f2da
xcanwin <14187349+xcanwin@users.noreply.github.com>
2024-05-22 04:20:12 +0800

    更新源

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           28.6
+// @version           28.7
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -1225,8 +1225,8 @@ nav.flex .transition-all {
     };
 
     const checkForUpdates = function(action = "click") {
-        const downloadURL = GM_info.script.downloadURL;
-        const updateURL = GM_info.scriptUpdateURL || GM_info.script.updateURL || downloadURL;
+        const downloadURL = `https://raw.githubusercontent.com/xcanwin/KeepChatGPT/main/KeepChatGPT.user.js`;
+        const updateURL = downloadURL;
         GM_xmlhttpRequest({
             method: "GET",
             url: `${updateURL}?t=${Date.now()}`,
