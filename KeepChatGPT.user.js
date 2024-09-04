ba362c4a25e6822e33c1e36e7f8ab6494f61edc0
未知可行 <14187349+xcanwin@users.noreply.github.com>
2024-09-04 14:40:08 +0800

    修复展示大屏bug

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           29.1
+// @version           29.2
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -566,7 +566,7 @@
             } else {
                 sv("k_largescreen", true);
             }
-            $("#__next .overflow-hidden.w-full>div.overflow-hidden").classList.toggle('largescreen');
+            $("main.w-full").classList.toggle('largescreen');
             $('.checkbutton', this).classList.toggle('checked');
         };
 
@@ -650,7 +650,7 @@
 
         if (gv("k_largescreen", false) === true) {
             $('#nmenuid_ls .checkbutton').classList.add('checked');
-            $("#__next .overflow-hidden.w-full>div.overflow-hidden").classList.toggle('largescreen');
+            $("main.w-full").classList.toggle('largescreen');
         }
 
         if (gv("k_speakcompletely", false) === true) {
