45238a5923f60938b80c48c2aa6aa3a5177827b6
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-03-04 05:03:11 +0800

    修复暗色

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           30.1
+// @version           30.2
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -789,6 +789,13 @@
         background-image: unset;
     }
 
+    nav.flex li::after {
+        content: "";
+        display: block;
+        height: 1px;
+        background: linear-gradient(to right, transparent, #bfbfbf, transparent);
+    }
+
     /*左边栏选中条目*/
     nav.flex li>div.bg-token-sidebar-surface-tertiary {
         background-color: #bfcbfd;
@@ -822,6 +829,24 @@
     .ever-changing {
         nav.flex {
             background: linear-gradient(to right top, #171717, #060606, #171717);
+            padding-right: .6rem;
+        }
+        nav.flex li>div {
+            height: 3.5rem;
+            background-color: rgba(111, 111, 111, 0.1);
+        }
+        nav.flex li>div>a {
+            mask-image: unset !important;
+        }
+        nav.flex li>div .bg-gradient-to-l {
+            background-image: unset;
+        }
+
+        nav.flex li::after {
+            content: "";
+            display: block;
+            height: 1px;
+            background: linear-gradient(to right, transparent, #535353, transparent);
         }
         nav.flex li>div.bg-token-sidebar-surface-tertiary {
             background-color: #444;
@@ -1057,12 +1082,6 @@ nav.flex div.overflow-y-auto {
     font-size: 0.7rem;
     font-weight: bold;
     color: white;
-}
-.knav li::after {
-    content: "";
-    display: block;
-    height: 1px;
-    background: linear-gradient(to right, transparent, #bfbfbf, transparent);
 }
 
 #nmenuid_ap {
