2111033f1a4619cff0b8a2649add89d7a6cdb07a
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-03-19 20:26:38 +0800

    绕过一部分CF机器人校验

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           30.3
+// @version           31.0
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -1469,7 +1469,19 @@ nav.flex .transition-all {
         }
     };
 
+    /*
+    绕过一部分CF机器人校验
+    */
+    const byebyeCF = () => {
+        GM_cookie.delete({
+            name: "cf_clearance",
+            domain: ".chatgpt.com",
+            path: "/"
+        });
+    };
+
     const nInterval1Fun = function() {
+        byebyeCF();
         if ($(symbol1_selector) || $(symbol2_selector)) {
             setIfr();
             speakCompletely();
