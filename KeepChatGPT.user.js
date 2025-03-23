2a96e4b99bd874ddeb04a2f3410845c6c38f7b0b
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-03-23 17:06:43 +0800

    修复明察秋毫bug

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           31.0
+// @version           31.1
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -966,7 +966,7 @@
     main div[data-message-author-role="user"] {
         padding-right: 3rem;
     }
-    main div[data-message-author-role="user"]>div.w-full>div {
+    main div[data-message-author-role="user"]>div.w-full>div>div {
         background-color: #deedd7;
     }
 
