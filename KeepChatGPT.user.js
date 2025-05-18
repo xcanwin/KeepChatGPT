d921eab54a833f657f979ff949a14b72c63f4c58
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-05-18 17:56:38 +0800

    修复日新月异

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           31.4
+// @version           31.5
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -742,7 +742,7 @@
         ndivkcg.setAttribute("class", "flex py-3 px-3 items-center gap-3 rounded-md text-sm mb-1 flex-shrink-0 border border-white/20");
 
         const icon = GM_info.script.icon ? GM_info.script.icon : `${GM_info.script.namespace}raw/main/assets/logo.svg`;
-        ndivkcg._symbol1_innerHTML = `<img src='${icon}' style='width: 1rem;' />Keep${ndivkcg.id.slice(1,2).toUpperCase()}hatGPT by x${ndivkcg.id.slice(1,2)}anwin`;
+        ndivkcg._symbol1_innerHTML = `<img src='${icon}' style='width: 1rem;' /><div style='font-size: 0.8rem'>Keep${ndivkcg.id.slice(1,2).toUpperCase()}hatGPT by x${ndivkcg.id.slice(1,2)}anwin</div>`;
         ndivkcg._symbol2_innerHTML = `<img src='${icon}' style='width: 1rem;' />`;
 
         if ($(symbol1_selector)) {
@@ -806,17 +806,11 @@
         background-color: #d5ddff;
     }
 
-    /*左边栏新聊天*/
-    nav .sticky.bg-token-sidebar-surface-primary {
-        background-color: unset !important;
-    }
-
-    /*左边栏分类*/
-    nav.flex div.overflow-y-auto .juice\\:sticky  {
-        background-color: unset !important;
-    }
-    nav.flex div.overflow-y-auto .relative.mt-5 {
-        margin-top: 0;
+    /*左边栏时间分类*/
+    @layer utilities {
+        .bg-token-bg-elevated-secondary {
+            background-color: unset !important;
+        }
     }
 
     /*左边栏日期*/
