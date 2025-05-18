dade84367317e0653e25ba1a94035c629c5d5ad1
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-05-18 18:14:28 +0800

    修复日新月异

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           31.5
+// @version           31.6
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -828,7 +828,7 @@
         }
         nav.flex li>div {
             height: 3.5rem;
-            background-color: rgba(111, 111, 111, 0.1);
+            background-color: rgba(111, 111, 111, 0.25);
         }
         nav.flex li>div>a {
             mask-image: unset !important;
@@ -851,7 +851,11 @@
         }
 
         nav.flex li a .navtitle {
-            color: #e1e1e1 !important;
+            color: #f4f4f4 !important;
+        }
+
+        nav.flex li a .navlast {
+            color: #d0d0d0 !important;
         }
     }
 }
