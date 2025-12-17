e4e5e099f64bec8bca34f5d6c0840103846d8759
未知可行 <14187349+xcanwin@users.noreply.github.com>
2025-12-18 04:12:25 +0800

    修复侧边栏色块问题

--- KeepChatGPT.user.js
+++ KeepChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              KeepChatGPT
 // @description       这是一款提高ChatGPT的数据安全能力和效率的插件。并且免费共享大量创新功能，如：自动刷新、保持活跃、数据安全、取消审计、克隆对话、言无不尽、净化页面、展示大屏、拦截跟踪、日新月异、明察秋毫等。让我们的AI体验无比安全、顺畅、丝滑、高效、简洁。
-// @version           32.8
+// @version           32.9
 // @author            xcanwin
 // @namespace         https://github.com/xcanwin/KeepChatGPT/
 // @supportURL        https://github.com/xcanwin/KeepChatGPT/
@@ -772,6 +772,14 @@
     nav.flex {
         background: linear-gradient(to right top, #d0dcff, #f0f0ff, #fff3f3);
     }
+    /*左边栏顶部选项*/
+    nav.flex .top-0 {
+        background: linear-gradient(to top, #f0f0ff, #fff3f3);
+    }
+    /*左边栏顶部工具*/
+    nav.flex aside {
+        background: linear-gradient(to top, #efebff, #f0f0ff);
+    }
     nav.flex #history>div {
         height: 3.5rem;
         background-color: rgba(255, 255, 255, 0.4);
@@ -818,6 +826,14 @@
     .ever-changing {
         nav.flex {
             background: linear-gradient(to right top, #171717, #060606, #171717);
+        }
+        /*左边栏顶部选项*/
+        nav.flex .top-0 {
+            background: linear-gradient(to top, #060606, #0f0f0f);
+        }
+        /*左边栏顶部工具*/
+        nav.flex aside {
+            background: linear-gradient(to top, #111, #060606);
         }
         nav.flex #history>div {
             height: 3.5rem;
